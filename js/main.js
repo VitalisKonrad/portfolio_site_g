3858b07d128e2dfee087bdac324369c310967723
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-08-20 17:56:11 +0300

    Add projects and swiper

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+	// speed: 400,
+	// spaceBetween: 100
+	pagination: {
+		el: '.project-pagination',
+		bulletClass: 'projects-bullets',
+		bulletActiveClass: 'projects-bullets-active',
+		clickable: true,
+	},
+});
+
